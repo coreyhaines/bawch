9a07eef6cbf1934b0f77326986a78bf2fa57433b
coreyhaines <coreyhaines@gmail.com>
2012-12-13 11:05:10 -0600

    The first step in world domination is making a commitment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoderetreatLive::Application.config.secret_token = 'ab3e9dfdea9d60cab2c18838aa9447aa6ed181317a4c5e24e0eade06443e47a7a11958efd9294ef8c12199882186a77031651c0981789cb64680beae3d17ec7c'
