6205f77c3523a07bdd947476162e2586ba4fed04
coreyhaines <coreyhaines@gmail.com>
2012-12-18 22:19:01 -0600

    Second scenario is passing with hard-coded html values

--- features/step_definitions/coderetreat_steps.rb
+++ features/step_definitions/coderetreat_steps.rb
@@ -1,6 +1,16 @@
 Given /^there are no coderetreats running today$/ do
 end
 
+Coderetreat = Struct.new :status, :location
+
+Given /^there are some coderetreats running today$/ do
+  @coderetreats = [
+    Coderetreat.new("not_started", "Chicago"),
+    Coderetreat.new("not_started", "Seattle"),
+    Coderetreat.new("in_session", "Berlin")
+  ]
+end
+
 When /^I go to the running coderetreats display page$/ do
   visit running_today_coderetreats_url
 end
@@ -9,11 +19,11 @@ Then /^I should see that there are no coderetreats running$/ do
   page.should have_content("There are no coderetreats running today")
 end
 
-Given /^there are some coderetreats running today$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
 Then /^I should see the running coderetreats grouped by status$/ do
-  pending # express the regexp above with the code you wish you had
+  @coderetreats.each do |coderetreat|
+    within(".#{coderetreat.status}") do
+      page.should have_css(".coderetreat", text: coderetreat.location)
+    end
+  end
 end
 
