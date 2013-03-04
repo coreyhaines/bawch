61e59daae910898b7278f14237ff26b11e68bd53
coreyhaines <coreyhaines@gmail.com>
2013-03-04 09:16:51 -0600

    Coderetreat.running_today returns all the coderetreats in the system

--- app/models/coderetreat.rb
+++ app/models/coderetreat.rb
@@ -2,6 +2,6 @@ class Coderetreat < ActiveRecord::Base
   attr_accessible :location, :status
 
   def self.running_today
-    []
+    all
   end
 end
--- spec/models/coderetreat/lookup_spec.rb
+++ spec/models/coderetreat/lookup_spec.rb
@@ -9,5 +9,11 @@ describe "Looking up coderetreats" do
         expect(Coderetreat.running_today).to be_empty
       end
     end
+
+    it "returns all the coderetreats in the system" do
+      in_session = Coderetreat.create! location: "Chicago", status: "in_session"
+      not_started = Coderetreat.create! location: "Seattle", status: "not_started"
+      expect(Coderetreat.running_today).to eq([in_session, not_started])
+    end
   end
 end
